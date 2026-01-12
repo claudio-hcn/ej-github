b7003581c2ed5d3a59dd021220d0b8c87999cc7c
Claudio Carrasco <claudio.hcn@outlook.com>
2026-01-12 13:41:01 -0300

    se agregan funciones al app.js y se modifica html

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 
+document.getElementById('btn-saludo').addEventListener('click', function () {
+    this.style.display = 'none';
+    document.getElementById('saludoForm').style.display = 'block';
+});
+
+document.getElementById('btnCancelar').addEventListener('click', function () {
+    document.getElementById('saludoForm').style.display = 'none';
+    document.getElementById('btn-saludo').style.display='';
+});
+
+document.getElementById('btnSaludar').addEventListener('click', function(){
+    nombre = document.getElementById('inputNombre');
+    console.log(nombre)
+    if(nombre.value!==""){
+        alert("hola " + nombre.value);
+       
+    }else{
+ alert("ingrese un nombre")
+    }
+    nombre.value = "";
+})
